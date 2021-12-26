7b40400d7f8f1326a1c60d2ab1ca2356ae0a5714
lbaitemple <lbai@temple.edu>
2021-12-25 21:56:29 -0500

    Update node_reading_laser.py

--- src/maze_demo/scripts/node_reading_laser.py
+++ src/maze_demo/scripts/node_reading_laser.py
@@ -4,15 +4,18 @@ import rospy
 
 from sensor_msgs.msg import LaserScan
 
+def avg(lst):
+    return sum(lst)/len(lst)
+
 def clbk_laser(msg):
     # 720 / 5 = 144
-    regions = [
-        min(min(msg.ranges[0:143]), 10),
-        min(min(msg.ranges[144:287]), 10),
-        min(min(msg.ranges[288:431]), 10),
-        min(min(msg.ranges[432:575]), 10),
-        min(min(msg.ranges[576:713]), 10),
-    ]
+    regions_ = {
+        'right':  min(avg(msg.ranges[0:10]),10.0),
+        'fright': min(avg(msg.ranges[30:40]),10.0),
+        'front':  min(avg(msg.ranges[85:95]),10.0),
+        'fleft':  min(avg(msg.ranges[140:150]),10.0),
+        'left':   min(avg(msg.ranges[170:180]),10.0),
+    	}
     rospy.loginfo(regions)
 
 def main():

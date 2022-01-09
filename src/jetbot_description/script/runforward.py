42b4118a3b4bca8f362343a81c6641b0dd7f3783
lbaitemple <lbai@temple.edu>
2022-01-08 22:13:39 -0500

    Add files via upload

--- src/jetbot_description/script/runforward.py
+++ src/jetbot_description/script/runforward.py
@@ -2,6 +2,7 @@
 
 import rospy
 from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseWithCovariance
@@ -9,27 +10,46 @@ from tf import transformations
 from std_srvs.srv import *
 from pid_tune.msg import PidTune
 from std_msgs.msg import Float32,String
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 
-rotation =0
+rotation_odom =0
+rotation_imu =0
 
-def clbk_laser(msg):
-    global rotation
+def clbk_odom(msg):
+    global rotation_odom
+    global roll_odom, pitch_odom, yaw_odom
     pose = msg.pose.pose
     pos_x=pose.position.x
     pos_y=pose.position.y
-    Kp=40
-    rotation = - Kp* pos_y
-    print(pos_y)
+    orientation_q = pose.orientation
+    orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+    (roll_odom, pitch_odom, yaw_odom) = euler_from_quaternion (orientation_list)
+    Kp=38
+    rotation_odom = - Kp*yaw_odom
+    print("Odometry yaw: ",yaw_odom)
+    
+    
+def clbk_imu(msg):
+    global rotation_imu
+    global roll_imu, pitch_imu, yaw_imu
+    orientation_q = msg.orientation
+    orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+    (roll_imu, pitch_imu, yaw_imu) = euler_from_quaternion (orientation_list)
+    Kp=38
+    rotation_imu = - Kp*yaw_imu
+    print(" \n IMU yaw: ",yaw_imu)
     
 
 def main():
-    global rotation
+    global rotation_odom
+    global rotation_imu
     rospy.init_node('reading_laser')
 
     pub_ = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
-    sub = rospy.Subscriber('/odom', Odometry, clbk_laser)
+    sub = rospy.Subscriber('/odom', Odometry, clbk_odom)
+    sub_imu = rospy.Subscriber('/imu', Imu, clbk_imu)
 
     rate = rospy.Rate(20)
     while not rospy.is_shutdown():
@@ -37,7 +57,7 @@ def main():
         
         msg = Twist()
         msg.linear.x = 0.1
-        msg.angular.z = rotation
+        msg.angular.z = rotation_imu
         
         pub_.publish(msg)
         rate.sleep()

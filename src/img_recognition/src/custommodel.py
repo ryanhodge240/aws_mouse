0bc66026a1030fade876f62a9a1a6de1f447f8ae
lbaitemple <lbai@temple.edu>
2022-02-18 21:40:06 -0500

    Update custommodel.py

--- src/img_recognition/src/custommodel.py
+++ src/img_recognition/src/custommodel.py
@@ -13,7 +13,7 @@ transformer = torchvision.transforms.Compose(
         torchvision.transforms.Resize((32, 32)),
         torchvision.transforms.RandomHorizontalFlip(p=0.5),
         torchvision.transforms.RandomVerticalFlip(p=0.5),
-#        torchvision.transforms.RandomRotation(30),
+        torchvision.transforms.RandomRotation(30),
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Normalize(
             mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]

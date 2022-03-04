fb68c7971d2b5061794cf4f1d986d4ec49a0ec04
lbaitemple <lbai@temple.edu>
2022-03-04 17:16:08 -0500

    Update installros1.sh

--- installros1.sh
+++ installros1.sh
@@ -3,9 +3,16 @@ sudo apt install curl -y # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 sudo apt update
-sudo apt install ros-noetic-desktop-full -y
+ver="$(lsb_release -sr)"
+if [ "$ver" = "20.04" ]
+  sudo apt install ros-noetic-desktop-full -y
+  echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+fi
+if [ "$ver" = "18.04" ]
+  sudo apt install ros-melodic-desktop-full -y
+  echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+fi
 
-echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 sudo apt install python3-rosdep -y

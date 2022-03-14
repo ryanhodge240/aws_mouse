53cf73e5e4e3d69bc0a1a8920a4f9ffd875589e1
lbaitemple <lbai@temple.edu>
2022-03-14 11:48:46 -0400

    Add files via upload

---
+++ src/maze_demo/scripts/maze_runner.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import sys
+import rospy
+from utils import Micromouse_Node
+
+wall_distance = 0.2
+wall_distance_forward = 0.15
+wall_distance_side = 0.1
+
+class MazeRunner(object):
+    def __init__(self):
+        rospy.init_node("mouse_node", anonymous=False)
+        micromouse_node = Micromouse_Node()
+        self.runMaze(micromouse_node)
+        rospy.on_shutdown(micromouse_node.on_shutdown)   
+        rospy.spin()    
+
+    def runMaze(self, mn):
+        # mn is the mouse node from MicroMouse_Node object
+        rospy.loginfo('starting....')
+        step = 0
+        while not rospy.is_shutdown():
+#            print(' left:front:right {:.3f}--{:.3f}--{:.3f}'.format(mn.laser_sensors['l'], mn.laser_sensors['f'], mn.laser_sensors['r']))
+            # if all three side are great thant the threshold, should go forward
+            if (mn.laser_sensors['l']>wall_distance_side):
+                #move forward distance one cell is 0.31 meters    
+                fdist=0.3
+                if  (mn.laser_sensors['f']>fdist):
+                    dist=mn.move_onecell(fdist)
+                    step = step + 1
+                    print("distance travelled in this cell is {:.4f}-{}".format(dist, step))
+                    print("classfication is {} ".format(mn.getPredication()))
+                else:
+                    dist = mn.laser_sensors['f'] 
+                    mn.move_onecell(dist)	
+
+
+if __name__ == "__main__" :
+    mr = MazeRunner()  
+    
+    
---
+++ src/mouse_smach/scripts/maze_fsm.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+from utils import Micromouse_Node
+import smach, smach_ros, rospy
+from smach import State, StateMachine,  CBState
+
+wall_distance = 0.2
+wall_distance_forward = 0.15
+wall_distance_side = 0.1
+
+class Maze_FSM(object):
+    def __init__(self):
+        rospy.init_node("mouse_node", anonymous=False)
+        self.mn = Micromouse_Node()
+        rospy.on_shutdown(self.mn.on_shutdown)   
+
+
+        self.FSM()
+               
+    def FSM(self):
+#    	rospy.init_node('fsm_py')
+
+  	# Create a SMACH state machine container
+    	sm = StateMachine(outcomes=['IDEL', 'succeeded', 'forward', 'left', 'right'])
+ 
+  	# Set user data for the finite state machine
+    	sm.userdata.lspeed = 0.2
+  	#sm.userdata.sm_input = input("Enter 1 to Push or 0 to Insert a Coin: ")
+     
+  	# Open the state machine container. A state machine container holds a number of states.
+    	with sm:
+    	    StateMachine.add('IDEL', CBState(self.cb_idel, cb_args=[self]),
+	    	{'succeeded':'IDEL','forward':'FORWARD', 'left':'LEFT', 'right':'RIGHT'})
+    	    StateMachine.add('FORWARD', CBState(self.cb_forward, cb_args=[self]),{'succeeded':'IDEL'})
+    	    StateMachine.add('LEFT', CBState(self.cb_left, cb_args=[self]), {'succeeded':'IDEL'})	
+    	    StateMachine.add('RIGHT', CBState(self.cb_right, cb_args=[self]),{'succeeded':'IDEL'})
+    	    StateMachine.add('DONE', CBState(self.cb_done, cb_args=[self]), {'succeeded':'IDEL'})	    		    	 
+	  # View our state transitions using ROS by creating and starting the instrospection server
+    	    sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
+    	    sis.start()
+	  # Execute the state machine 
+    	    outcome = sm.execute()
+ 
+	  # Wait for ctrl-c to stop the application
+    	    rospy.spin()
+    	    sis.stop()
+
+
+    
+    @smach.cb_interface(input_keys=['lspeed'], outcomes=['succeeded', 'IDEL', 'forward', 'left', 'right'])
+    def cb_idel(user_data, self):
+    	if (self.mn.laser_sensors['f']>wall_distance_forward):
+   	    return 'forward'
+    	else:
+   	    return 'IDEL'
+
+    @smach.cb_interface(input_keys=['lspeed'], output_keys=[], outcomes=['succeeded'])
+    def cb_forward(user_data, self):
+    	if (self.mn.laser_sensors['f']>wall_distance_forward):
+    	    self.mn.move_onecell(0.32)
+    	    return 'succeeded'
+   	    
+    @smach.cb_interface(input_keys=[], output_keys=[], outcomes=['succeeded'])
+    def cb_left(user_data, self):
+        pass
+    @smach.cb_interface(input_keys=[], output_keys=[], outcomes=['succeeded'])
+    def cb_right(user_data, self):
+        pass
+    @smach.cb_interface(input_keys=[], output_keys=[], outcomes=['succeeded'])
+    def cb_done(user_data, self):
+        pass                
+
+if __name__ == "__main__" :
+  
+    mf= Maze_FSM()
+    
+    

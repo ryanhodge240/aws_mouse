d52e9072442a0606975c66e65e80e51b362e1319
Ryan Hodge <Ryanhodge240@gmail.com>
2022-04-26 18:06:06 -0400

    Added and started the navigation node for the micromouse

--- src/maze_demo/scripts/mm_movement.py
+++ src/maze_demo/scripts/mm_movement.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python
+'''A node for the navigation of the MicroMouse'''
 
-# Importing packages will allow us to use specific functions to simplify our code
-# sys: Allows us to interact more closely with the interpreter
 import sys
-# rospy: Allows for interaction with ROS nodes and topics 
 import rospy
-# Micromouse_Node: A Node to manage Micromouse behavior. More can be found on this in the maze_demo/src/mouse_common/utils.py file
-from mouse_common.utils import Micromouse_Node
+import numpy as np
+from math import cos, sin
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Imu
+from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import Range
+from nav_msgs.msg import Odometry
+from img_recognition.msg import Prediction
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
+np.set_printoptions(precision=2)
+foundHeading = False
+current_heading =0
+orbit = 0
+laser_sensors = {'l': 0, 'fl': 0, 'f': 0, 'fr': 0, 'r': 0}
+
+linear_vel = 0.2
+angular_vel = 0.2
+wall_distance = 0.2
+wall_distance_forward = 0.30
+wall_distance_side = 0.15
+rotation_imu =0
+
+inf = float('inf')
+
+left = -1
+going_left = -2
+right = 1
+going_right = 2
 
 # Minimum Safety Range to Wall
 wall_distance = 0.2
@@ -21,97 +45,123 @@ wall_distance_forward = 0.15
 # Allowable Distance to Stop Moving in Other Directions
 wall_distance_side = 0.1
 
-class MazeRunner(object):
-    
-    # A Python Class creates an object that we can provide with methods. These methods will allow our object to perform some tasks. 
-    # MazeRunner is the name of our class, and we can instantiate a MazeRunner Object with the properties defined in the __init__
-    
-    def __init__(self):
+def calculate_lasers_range(data):
+    '''Dynamic range intervals'''
+    global laser_sensors
+    half_pi = np.pi / 2
+    initial_angle = 0  
+    final_angle = 0   
+    if data.angle_min < -half_pi:
+        default_min_angle = half_pi / data.angle_increment
+        robot_initial_angle = -data.angle_min / data.angle_increment
+        initial_angle = robot_initial_angle - default_min_angle
+    if data.angle_max > np.pi / 2:
+        default_max_angle = half_pi / data.angle_increment
+        robot_final_angle = data.angle_max / data.angle_increment
+        final_angle = robot_final_angle - default_max_angle
 
-        # Every Python class must have an __init__(self) method to provide every object of that type with a set of properties
+    laser_interval = (len(data.ranges) - initial_angle - final_angle) / 2
+    half_laser_interval = laser_interval / 4
+    initial_angle = final_angle - 4*laser_interval
+#    laser_interval = 45
+#    half_laser_interval = 22.5
+#    rospy.loginfo("\n\n=------> ref : %s %s  %s\n\n",final_angle, laser_interval,initial_angle)
 
-        # Start Mouse Node 
-        rospy.init_node("mouse_node", anonymous=False)
+    interval = [None] * 5
+    interval[0] = np.mean(data.ranges[int(initial_angle- half_laser_interval):int(initial_angle+half_laser_interval)])
+    interval[0] = min(interval[0], 8.0)
+ #   rospy.loginfo("\n\n=------> len : %s  %s\n\n",int(initial_angle- half_laser_interval),int(initial_angle+half_laser_interval))
 
-        # Instantiate a new copy of the Micromouse_Node object 
-        micromouse_node = Micromouse_Node()
+    for i in range(1, 5):
+        dirty_values = data.ranges[int(
+            initial_angle + i * laser_interval - half_laser_interval
+        ):int(initial_angle + i * laser_interval + half_laser_interval) + 1]
+#        rospy.loginfo("=------> lser : %s  %s\n",int(initial_angle + i * laser_interval - half_laser_interval),int(initial_angle + i * laser_interval + half_laser_interval) )
+        interval[i] = min(np.mean(np.nan_to_num(dirty_values)), 8.0)
 
-        # Rune Maze with out new object 
-        self.runMaze(micromouse_node)
-        
-        # Shut down node when finished
-        rospy.on_shutdown(micromouse_node.on_shutdown)   
+    laser_sensors['r'] = interval[0]
+    laser_sensors['fr'] = interval[1]
+    laser_sensors['f'] = interval[2]
+    laser_sensors['fl'] = interval[3]
+    laser_sensors['l'] = interval[4]
 
-        # Require ctrl+c to cancel process
-        rospy.spin()    
+def get_velocity_message(turn_left, turn_right, move_forward):
+    '''Take in instructions for the type of velocity to put on the robot'''
+    global laser_sensors, rotation_imu, foundHeading
+    angular = 0
+    linear = 0
+    turn =""
+
+    if turn_left:
+    	#turn left based on current oritenation
+        angular = rotation_imu + 1.57
+        foundHeading = False  # need to update heading
+        turn += "Turn Left | "
+        #linear = linear_vel*0.2
+    if turn_right:
+        angular = rotation_imu -1.57
+        turn += "Turn Right | "
+       # linear = linear_vel*0.2
+    if move_forward:
+        linear = linear_vel
+        angular = rotation_imu
+        turn += "Move Forward   "
+
+    vel_msg = Twist()
+    vel_msg.linear.x = linear
+    vel_msg.angular.z = angular
+
+    rospy.loginfo("Movement: linear %s angular %s - Direction %s ",linear, angular, turn)
+
+    return vel_msg
+
+def laser_callback(laser_msg):
+    '''Callback function for receiving laser messages to publish velocity messages'''
+    global laser_sensors
+    turn_left = False
+    turn_right = False
+    move_forward = False
+
+    calculate_lasers_range(laser_msg)
+
+    if laser_sensors['r'] > turn_distance:
+        turn_right = True
+    if laser_sensors['f'] > wall_distance_forward:
+        move_forward = True
+    if laser_sensors['l'] > turn_distance:
+        turn_left = True
+
+    vel_msg = get_velocity_message(turn_left, turn_right, move_forward)
+    vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    vel_pub.publish(vel_msg)
 
 
-    def runMaze(self, mn):
-        # Inputs: 
-            # self: MazeRunner Object
-            # mn: Mouse Node from MazeRunner Object
-        # Function:
-            # While rospy is running this method will check the left and front laser scan topics
-            # and decide to move forward one cell or rotate 
+def imu_callback(imu_msg):
+    '''Callback used for the IMU sensor'''
+    global rotation_imu, current_heading, foundHeading
+    global roll_imu, pitch_imu, yaw_imu
+    orientation_q = imu_msg.orientation
+    orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+    (roll_imu, pitch_imu, yaw_imu) = euler_from_quaternion(orientation_list)
+    if not foundHeading:
+        current_heading = yaw_imu
+        foundHeading = True
+    Kp=38
+    rotation_imu =  Kp*(current_heading-yaw_imu)
+    print(" \n IMU yaw: ", yaw_imu)
 
-        # Log Info 
-        rospy.loginfo('starting....')
-
-        # Start Counting
-        step = 0
-
-        # While ROS is running
-        while not rospy.is_shutdown():
-            # Set Distance Moved With Each Forward Step  
-            fdist = 0.3
-            
-            # # if all three side are great thant the threshold, should go forward
-            # if (mn.laser_sensors['left']>wall_distance_side):
-            #     # This case statement will select what to do given sensor data 
-            #     if  (mn.laser_sensors['front']>fdist):
-            #         # When there is room asdin front
-            #         # Use the move_oncecell function of our mn object
-            #         dist=mn.move_onecell(fdist)
-            #         # Add a count 
-            #         step = step + 1
-            #         # Log this info to screen
-            #         print("distance travelled in this cell is {:.4f}-{}".format(dist, step))
-            #         print("classfication is {} ".format(mn.getPredication()))
-
-            #     # What can we do when the laser detects a wall in front of us?
-            #     else:
-            #         dist = mn.laser_sensors['front'] 
-            #         mn.move_onecell(dist)
-                    
-            # Try to turn right
-            if (mn.laser_sensors['right'] > turn_distance):
-                print('right')
-                mn.turnangle(target = -90)
-                mn.move_onecell(fdist)
-            
-            # If you can't turn right, go forward
-            elif (mn.laser_sensors['front'] > fdist):
-                print('forward')
-                mn.move_onecell(fdist)
-                
-            # If you cant go forward or right, try left
-            elif (mn.laser_sensors['left'] > turn_distance):
-                print('left')
-                mn.turnangle(target = 90)
-
-            # Otherwise, turn around
-            else:
-                print('back')
-                mn.turnangle(target = 180)
+def prediction_callback(prediction):
+    '''Callback used for the prediction algorithm'''
+    print('Prediction')
 
 def listeners():
+    '''Setup all the listeners then wait for ROS to die'''
     # subscribe and read scan message, check laser_callback function
     rospy.Subscriber('/scan', LaserScan, laser_callback)
     # subscribe imu message (oritenation of the robot)
-    rospy.Subscriber(imu_callback)
+    rospy.Subscriber('/imu', Imu, imu_callback)
     #subscribe to predication topic
     rospy.Subscriber("/prediction", Prediction, prediction_callback)
-   
     rospy.spin()
 
 
@@ -119,6 +169,3 @@ def listeners():
 if __name__ == "__main__" :
     rospy.init_node('avoid_wall_1', anonymous=True)
     listeners()
-    mr = MazeRunner()
-    
-    

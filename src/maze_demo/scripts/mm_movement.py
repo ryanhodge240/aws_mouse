12a7aa0b4e3e2ae1a8ede1ac229d7c58372ce6a9
Ryan Hodge <Ryanhodge240@gmail.com>
2022-04-29 07:02:31 +0000

    Turning working slowly

--- src/maze_demo/scripts/mm_movement.py
+++ src/maze_demo/scripts/mm_movement.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 '''A node for the navigation of the MicroMouse'''
 
+from __future__ import division
 import sys
 import rospy
 import numpy as np
-from math import cos, sin
+import math
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Imu
 from sensor_msgs.msg import LaserScan
@@ -12,198 +13,182 @@ from sensor_msgs.msg import Range
 from nav_msgs.msg import Odometry
 from img_recognition.msg import Prediction
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from mouse_common.utils import Micromouse_Node
 
 np.set_printoptions(precision = 2)
-foundHeading = False
-target_complete = True
-target_angle = 0
-is_turning = False
 
-current_heading = 0
-orbit = 0
-laser_sensors = {'l': 0, 'fl': 0, 'f': 0, 'fr': 0, 'r': 0}
+# Minimum distance from walls on sides and in front
+WALL_DIST = 0.12
+FORWARD_WALL_DIST = 0.2
 
-linear_vel = 0.3
-angular_vel = 0.5
-wall_distance = 0.2
-wall_distance_forward = 0.30
-wall_distance_side = 0.15
-rotation_imu = 0
+# Normal linear and angular velocity speed
+LIN_VEL = 0.3
+ANG_VEL = 0.3
 
-inf = float('inf')
-
-left = -1
-going_left = -2
-right = 1
-going_right = 2
-
-# Minimum Safety Range to Wall
-wall_distance = 0.2
-
-# Distance where you should be able to turn
-turn_distance = 0.5
-
-# Allowable Distance to Stop Moving Forward
-wall_distance_forward = 0.15
-
-# Allowable Distance to Stop Moving in Other Directions
-wall_distance_side = 0.1
+# The tolerance for turning (degrees)
+TOL = 4
 
 def is_within_tolerance(num1, num2, tolerance):
     '''Check if the first number is within a specified tolerance of another'''
-    print('Target: ', num1, ' Val: ', num2)
-    print(abs(num1 - num2) < tolerance)
     return abs(num1 - num2) < tolerance
 
-def calculate_lasers_range(data):
-    '''Dynamic range intervals'''
-    global laser_sensors
-    half_pi = np.pi / 2
-    initial_angle = 0
-    final_angle = 0
+#TODO: Find out what this does
+def pi_to_pi(angle):
+    '''Convert somehting'''
+    angle = np.mod(angle, (2*np.pi))
+    if angle>np.pi:
+        angle = angle - (2*np.pi)
+    elif angle<(-np.pi):
+        angle = angle + (2*np.pi)
+        
+    return angle
 
-    if data.angle_min < -half_pi:
-        default_min_angle = half_pi / data.angle_increment
-        robot_initial_angle = -data.angle_min / data.angle_increment
-        initial_angle = robot_initial_angle - default_min_angle
-    if data.angle_max > np.pi / 2:
-        default_max_angle = half_pi / data.angle_increment
-        robot_final_angle = data.angle_max / data.angle_increment
-        final_angle = robot_final_angle - default_max_angle
+class MazeRunner(object):
+    '''MazeRunner python class object for running the MicroMouse'''
 
-    laser_interval = (len(data.ranges) - initial_angle - final_angle) / 2
-    half_laser_interval = laser_interval / 4
-    initial_angle = final_angle - 4 * laser_interval
-#    laser_interval = 45
-#    half_laser_interval = 22.5
-#    rospy.loginfo("\n\n=------> ref : %s %s  %s\n\n",final_angle, laser_interval,initial_angle)
+    def __init__(self):
+        # Start Mouse Node
+        self.found_heading = False
+        self.current_heading = 0
+        self.laser_sensors = {'l': 0, 'fl': 0, 'f': 0, 'fr': 0, 'r': 0}
+        self.rotation_imu = 0
+        self.yaw_imu = 0
 
-    interval = [None] * 5
-    interval[0] = np.mean(data.ranges[int(initial_angle- half_laser_interval):int(initial_angle+half_laser_interval)])
-    interval[0] = min(interval[0], 8.0)
- #   rospy.loginfo("\n\n=------> len : %s  %s\n\n",int(initial_angle- half_laser_interval),int(initial_angle+half_laser_interval))
+        # Initialize publisher
+        self.vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
-    for i in range(1, 5):
-        dirty_values = data.ranges[int(
-            initial_angle + i * laser_interval - half_laser_interval
-        ):int(initial_angle + i * laser_interval + half_laser_interval) + 1]
-#        rospy.loginfo("=------> lser : %s  %s\n",int(initial_angle + i * laser_interval - half_laser_interval),int(initial_angle + i * laser_interval + half_laser_interval) )
-        interval[i] = min(np.mean(np.nan_to_num(dirty_values)), 8.0)
+        # Initialize subscribers
+        self.listeners()
 
-    laser_sensors['r'] = interval[0]
-    laser_sensors['fr'] = interval[1]
-    laser_sensors['f'] = interval[2]
-    laser_sensors['fl'] = interval[3]
-    laser_sensors['l'] = interval[4]
+        # Require ctrl+c to cancel process
+        rospy.spin()
 
-def get_velocity_message(turn_left, turn_right, move_forward, turn_around):
-    '''Take in instructions for the type of velocity to put on the robot'''
-    global laser_sensors, rotation_imu, foundHeading, target_complete, target_angle, is_turning
-    target_angle = 0
-    linear_velocity = 0
-    turn = ""
-    target_complete = True
-    is_turning = True
-    angular_veloity = 0
+    def listeners(self):
+        '''Setup all the listeners then wait for ROS to die'''
+        # subscribe and read scan message, check laser_callback function
+        rospy.Subscriber('/scan', LaserScan, self.laser_callback)
+        # subscribe imu message (oritenation of the robot)
+        rospy.Subscriber('/imu', Imu, self.imu_callback)
+        #subscribe to predication topic
+        rospy.Subscriber("/prediction", Prediction, self.prediction_callback)
 
-    if turn_right:
-        angular_veloity = -angular_vel
-        target_angle = rotation_imu + 90
-        turn += "Turn Right | "
-        target_complete = False
-    elif turn_left:
-    	#turn left based on current oritenation
-        angular_veloity = angular_vel
-        target_angle = rotation_imu - 90
-        turn += "Turn Left | "
-        target_complete = False
-    elif turn_around:
-        angular_veloity = angular_vel
-        target_angle = rotation_imu - 180
-        turn += "Turn Around | "
-        target_complete = False
-    elif move_forward:
-        is_turning = False
-        linear_velocity = linear_vel
-        target_angle = rotation_imu
-        turn += "Move Forward   "
+    def imu_callback(self, imu_msg):
+        '''Callback used for the IMU sensor'''
+        orientation_q = imu_msg.orientation
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        (_, _, self.yaw_imu) = euler_from_quaternion(orientation_list)
+        if not self.found_heading:
+            self.current_heading = self.yaw_imu
+            self.found_heading = True
+        k_p = 38
+        self.rotation_imu = k_p * (self.current_heading - self.yaw_imu)
+        # print(" \n IMU yaw: ", yaw_imu)
 
-    vel_msg = Twist()
-    vel_msg.linear.x = linear_velocity
-    vel_msg.angular.z = target_angle
+    def prediction_callback(self, prediction):
+        '''Callback used for the prediction algorithm'''
+        print('Prediction')
 
-    # rospy.loginfo("Movement: linear %s angular %s - Direction %s ", linear_velocity, target_angle, turn)
+    def laser_callback(self, laser_msg):
+        '''Callback function for receiving laser messages to publish velocity messages'''
+        self.calculate_lasers_range(laser_msg)
 
-    return vel_msg
+        if self.laser_sensors['f'] >= FORWARD_WALL_DIST:
+            self.forward()
+        elif self.laser_sensors['l'] >= WALL_DIST:
+            self.left()
 
-def laser_callback(laser_msg):
-    '''Callback function for receiving laser messages to publish velocity messages'''
-    global laser_sensors, target_complete, is_turning, target_angle
-    turn_left = False
-    turn_right = False
-    move_forward = False
-    turn_around = False
+    def calculate_lasers_range(self, data):
+        '''Dynamic range intervals'''
+        half_pi = np.pi / 2
+        initial_angle = 0
+        final_angle = 0
 
-    calculate_lasers_range(laser_msg)
+        if data.angle_min < -half_pi:
+            default_min_angle = half_pi / data.angle_increment
+            robot_initial_angle = -data.angle_min / data.angle_increment
+            initial_angle = robot_initial_angle - default_min_angle
+        if data.angle_max > np.pi / 2:
+            default_max_angle = half_pi / data.angle_increment
+            robot_final_angle = data.angle_max / data.angle_increment
+            final_angle = robot_final_angle - default_max_angle
 
-    if is_within_tolerance(target_angle, rotation_imu, 4):
-        target_complete = True
-        is_turning = False
+        laser_interval = (len(data.ranges) - initial_angle - final_angle) / 2
+        half_laser_interval = laser_interval / 4
+        initial_angle = final_angle - 4 * laser_interval
 
-    if target_complete:
-        print('Target complete')
+        interval = [None] * 5
+        interval[0] = np.mean(data.ranges[int(initial_angle- half_laser_interval):int(initial_angle+half_laser_interval)])
+        interval[0] = min(interval[0], 8.0)
 
-        if laser_sensors['r'] > turn_distance:
-            turn_right = True
-        elif laser_sensors['f'] > wall_distance_forward:
-            move_forward = True
-        elif laser_sensors['l'] > turn_distance:
-            turn_left = True
+        for i in range(1, 5):
+            dirty_values = data.ranges[int(
+                initial_angle + i * laser_interval - half_laser_interval
+            ):int(initial_angle + i * laser_interval + half_laser_interval) + 1]
+            interval[i] = min(np.mean(np.nan_to_num(dirty_values)), 8.0)
+
+        self.laser_sensors['r'] = interval[0]
+        self.laser_sensors['fr'] = interval[1]
+        self.laser_sensors['f'] = interval[2]
+        self.laser_sensors['fl'] = interval[3]
+        self.laser_sensors['l'] = interval[4]
+
+    def left(self):
+        '''Turn the robot left'''
+
+        print('Turn left!!!')
+        target_rad = pi_to_pi(np.radians(90) + self.current_heading)
+        k_p = 2
+
+        while not rospy.is_shutdown():
+            if is_within_tolerance(target_rad, self.yaw_imu, 0.01):
+                self.found_heading = False   # updated heading after the rotation
+                break
+            else:
+                vel_msg = Twist()
+                vel_msg.angular.z = k_p * pi_to_pi(target_rad - self.yaw_imu)
+                self.vel_publisher.publish(vel_msg)
+
+    def right(self):
+        '''Turn the robot right'''
+
+    def back(self):
+        '''Turn the robot around'''
+
+    def forward(self):
+        '''Move the robot forward'''
+        # Send out desired velocity
+        self.vel_publisher.publish(self.follow_right_wall())
+
+    def follow_right_wall(self, mv_forward = True, desired_dist = 0.14, k_p = 111):
+        '''Follow the right wall to keep straight *MAGIC*'''
+        # pylint: disable=invalid-name
+        theta = 45
+        sign = 1
+
+        if mv_forward is True:
+            ac = LIN_VEL
         else:
-            turn_around = True
+            ac = -LIN_VEL
+            k_p=k_p/10
+            sign=-1
 
-        vel_msg = get_velocity_message(turn_left, turn_right, move_forward, turn_around)
+        a = self.laser_sensors['fr']
+        b = self.laser_sensors['r']
+        swing = math.radians(theta)
+        ab_angle = sign*math.atan2( a * math.cos(swing) - b , a * math.sin(swing))
+        ab = b * math.cos(ab_angle)
 
-        # Publish to the velocity msg
-        vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-        vel_pub.publish(vel_msg)
+        cd = ab + ac * math.sin(ab_angle)
+        error = cd - desired_dist
 
+        msg = Twist()
+        output = -sign * k_p * error
+        msg.linear.x = ac
+        msg.angular.z =  output
 
-def imu_callback(imu_msg):
-    '''Callback used for the IMU sensor'''
-    global rotation_imu, current_heading, foundHeading
-    global roll_imu, pitch_imu, yaw_imu, is_turning, target_complete, target_angle
-    
-    # if is_turning and is_within_tolerance(target_angle, rotation_imu, 0.3):
-    #     target_complete = True
-    #     is_turning = False
-    
-    orientation_q = imu_msg.orientation
-    orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
-    (roll_imu, pitch_imu, yaw_imu) = euler_from_quaternion(orientation_list)
-    if not foundHeading:
-        current_heading = yaw_imu
-        foundHeading = True
-    Kp=38
-    rotation_imu = Kp*(current_heading-yaw_imu)
-    # print(" \n IMU yaw: ", yaw_imu)
-
-def prediction_callback(prediction):
-    '''Callback used for the prediction algorithm'''
-    print('Prediction')
-
-def listeners():
-    '''Setup all the listeners then wait for ROS to die'''
-    # subscribe and read scan message, check laser_callback function
-    rospy.Subscriber('/scan', LaserScan, laser_callback)
-    # subscribe imu message (oritenation of the robot)
-    rospy.Subscriber('/imu', Imu, imu_callback)
-    #subscribe to predication topic
-    rospy.Subscriber("/prediction", Prediction, prediction_callback)
-    rospy.spin()
-
+        return msg
 
 # Run the MazeRunner
 if __name__ == "__main__" :
-    rospy.init_node('avoid_wall_1', anonymous=True)
-    listeners()
+    rospy.init_node('mouse_node', anonymous=True)
+    mr = MazeRunner()
